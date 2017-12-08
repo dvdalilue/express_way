fed108a81268c3f2f748c111dbb6ba2bff2bda09
David Lilue <dvdalilue@gmail.com>
2017-12-08 15:30:31 -0400

    Event class for every line in the csv file

---
+++ src/main/java/master2017/flink/Event.java
@@ -0,0 +1,66 @@
+package master2017.flink;
+
+import org.apache.flink.api.java.tuple.Tuple8;
+import org.apache.flink.api.common.functions.MapFunction;
+
+public final class Event {
+    public int time;
+    public int vid;
+    public int speed;
+    public int xWay;
+    public int lane;
+    public int dir;
+    public int seg;
+    public int pos;
+
+    public Event() { }
+
+    public Event(int f0, int f1, int f2, int f3, int f4, int f5, int f6, int f7) {
+        this.time  = f0;
+        this.vid   = f1;
+        this.speed = f2;
+        this.xWay  = f3;
+        this.lane  = f4;
+        this.dir   = f5;
+        this.seg   = f6;
+        this.pos   = f7;
+    }
+
+    public Event(Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer> t) {
+        this.time = t.f0;
+        this.vid = t.f1;
+        this.speed = t.f2;
+        this.xWay = t.f3;
+        this.lane = t.f4;
+        this.dir = t.f5;
+        this.seg = t.f6;
+        this.pos = t.f7;
+    }
+
+    public Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer> toTuple() {
+        return (new Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer>(
+            time,
+            vid,
+            speed,
+            xWay,
+            lane,
+            dir,
+            seg,
+            pos
+        ));
+    }
+
+    public static final class toEvent implements MapFunction<Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer>, Event> {
+        @Override
+        public Event map(Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer> in) {
+            return (new Event(in));
+        }
+    }
+
+    public static final class toTuple implements MapFunction<Event, Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer>> {
+        @Override
+        public Tuple8<Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer> map(Event in) {
+            return (in.toTuple());
+        }
+    }
+}
