76afa0e6fbe7f73689111b4b1e94be293d81a5f4
David Lilue <dvdalilue@gmail.com>
2017-12-14 20:01:56 +0100

    Average speed control inside segment with DataStream

--- src/main/java/master2017/flink/VehicleTelematics.java
+++ src/main/java/master2017/flink/VehicleTelematics.java
@@ -7,10 +7,20 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.util.Collector;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -25,6 +35,8 @@ public class VehicleTelematics {
         // set up the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
         DataStream<Event> events =
             env.readTextFile(args[0])
 
@@ -32,43 +44,57 @@ public class VehicleTelematics {
 
             .map(new Event.toEvent());
 
-            // .pojoType(
-            //     Event.class,
-            //     "time",
-            //     "vid",
-            //     "speed",
-            //     "xWay",
-            //     "lane",
-            //     "dir",
-            //     "seg",
-            //     "pos"
-            // );
+            // .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Event>() {
+            //     @Override
+            //     public long extractAscendingTimestamp(Event e) {
+            //         return (e.time * 1000);
+            //     }
+            // });
 
         /**
          * Speed Radar
          */
         
-        events
+        // events
 
-        .flatMap(new SpeedRadar())
+        // .windowAll(TumblingEventTimeWindows((long)100,(long)0))
 
-        .writeAsCsv(args[1] + "/speedfines.csv", FileSystem.WriteMode.OVERWRITE, "\n", ",");
+        // .flatMap(new SpeedRadar())
+
+        // .keyBy(new SelectorVID())
+
+        // .window(TumblingEventTimeWindows.of(Time.seconds(100)))
+
+        // .apply(new WindowFunction<Event, SpeedFine, Integer, TimeWindow>() {
+        //     public void apply(Integer key, TimeWindow window, Iterable<Event> events, Collector<SpeedFine> out) {
+        //         System.out.println("-------> " + key);
+        //         // for (Event e : events) {
+        //         //     if (e.speed > 90) { out.collect(new SpeedFine(e.time, e.vid, e.xWay, e.seg, e.dir, e.speed)); }
+        //         // }
+        //     }
+        // })
+
+        // .writeAsCsv(args[1] + "/speedfines.csv", FileSystem.WriteMode.OVERWRITE, "\n", ",");
         
         /**
          * Average Speed Control Segment
          */
         
-        // events
+        events
 
-        // .filter(new InsideControlSegment())
+        .filter(new InsideControlSegment())
 
-        // .groupBy(new SelectorVID())
+        .map(new ToAccSpeedFine())
 
-        // .reduceGroup(new AvgInCompleteSegment())
+        .keyBy(new AccSpeedFine.SelectorVID())
 
-        // .filter(new AboveAvgSpeedSegment())
+        .reduce(new MergeAccSpeedFine())
 
-        // .writeAsCsv(args[1] + "/avgspeedfines.csv", "\n", ",", FileSystem.WriteMode.OVERWRITE);
+        .flatMap(new InCompleteSegment())
+
+        .filter(new AboveAvgSpeedSegment())
+
+        .writeAsCsv(args[1] + "/avgspeedfines.csv", FileSystem.WriteMode.OVERWRITE, "\n", ",");
 
         /**
          * Accidents
@@ -157,6 +183,27 @@ public class VehicleTelematics {
         }
     }
 
+    public static final class InCompleteSegment
+    implements FlatMapFunction<AccSpeedFine,AvgSpeedFine> {
+        @Override
+        public void flatMap(AccSpeedFine e, Collector<AvgSpeedFine> out) {
+            int[] all_segments = {52,53,54,55,56};
+
+            boolean inSegment = true;
+
+            for (int i = 0; i < all_segments.length && inSegment == true; i++) {
+                if (!e.segments.contains(new Integer(all_segments[i]))) {
+                    inSegment = false;
+                }
+            }
+
+            if (inSegment) {
+                int average = e.speed / e.n_event;
+                out.collect(new AvgSpeedFine(e.time1,e.time2,e.vid,e.xWay,e.dir,average));
+            }
+        }
+    }
+
     /**
      * Implements the filter function to keep the events with the speed above
      * the limit (60). It's used to check the avergae speed within a specific
@@ -184,6 +231,24 @@ public class VehicleTelematics {
         }
     }
 
+    public static final class MergeAccSpeedFine
+    implements ReduceFunction<AccSpeedFine> {
+        @Override
+        public AccSpeedFine reduce(AccSpeedFine e1, AccSpeedFine e2) {
+            if (e1.time1 > e2.time1) { e1.time1 = e2.time1; } // Min time
+
+            if (e1.time2 < e2.time2) { e1.time2 = e2.time2; } // Max time
+
+            e1.speed += e2.speed; // Accumulated speed
+
+            e1.n_event += 1; // Events counter
+
+            e1.segments.addAll(e2.segments); // Segments visited
+
+            return e1;
+        }
+    }
+
     /**
      * Implements a key selector for the Event class. The unique identifier for
      * each event is the vehicle id (vid). 
@@ -197,9 +262,9 @@ public class VehicleTelematics {
     }
 
     public static class SelectorTime
-    implements KeySelector<Event, Integer> {
+    implements KeySelector<Event, Long> {
         @Override
-        public Integer getKey(Event e) {
+        public Long getKey(Event e) {
             return e.time;
         }
     }
@@ -209,6 +274,14 @@ public class VehicleTelematics {
         @Override
         public VIDTime getKey(Event e) {
             return (new VIDTime(e.vid,e.time));
+        }
+    }
+
+    public static class ToAccSpeedFine
+    implements MapFunction<Event,AccSpeedFine> {
+        @Override
+        public AccSpeedFine map(Event e) {
+            return new AccSpeedFine(e);
         }
     }
 
@@ -226,8 +299,8 @@ public class VehicleTelematics {
 
             Event current = it.next();
 
-            int time_min = current.time;
-            int time_max = current.time;
+            long time_min = current.time;
+            long time_max = current.time;
             int vid = current.vid;
             int xWay = current.xWay;
             int dir = current.dir;
